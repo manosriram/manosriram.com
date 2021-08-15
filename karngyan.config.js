5d9e6831ee564f6481197835ba93b1cef7ee5612
Mano Sriram <mano.sriram0@gmail.com>
2021-08-16 02:13:22 +0530

    Remove images

--- karngyan.config.js
+++ karngyan.config.js
@@ -55,10 +55,7 @@ export default {
     // add logos in static and at max add 3/4
     enabled: false,
     meta: [
-      { name: 'SendPost', src: '/images/sendpost.png', url: 'https://sendpost.io' },
-      { name: 'Amazon', src: '/images/amazon.png', url: 'https://amazon.in' },
-      { name: 'InterviewReady', src: '/images/interviewready-io.png', url: 'https://get.interviewready.io' },
-      { name: 'CrioDo', src: '/images/crio.png', url: 'https://crio.do' },
+      { name: 'Company1', src: '/images/sendpost.png', url: 'https://sendpost.io' },
     ]
   },
   loadingIndicator: {
